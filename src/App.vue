5aeab29d23d7922e4d81c0693a128b4f0e78115c
DouradoCtrl <gdouradoestudante@gmail.com>
2026-02-04 16:55:51 -0300

    add: AULA 05: aprendendo a utilizar v-for e v-bind:key

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,70 @@
 <template>
-  <TheHeader v-if="showHeader"/>
-
-  <div v-show="showName">
-    Nome: {{ firstName }} <br/>
-    Sobrenome: {{ lastName }}
+  <div 
+    v-for="obj in todos" 
+    v-bind:key="obj.id"
+    class="todos-item"
+  > 
+    <!-- obj seria o objeto numero 1 do array -->
+    <!-- precisa passar uma key em sistema robusto -->
+    {{ obj.title }}
   </div>
-
-  <div v-if="acessLevel === 'admin'">Usuário Admin</div>
-  <div v-else-if="acessLevel === 'marketing'">Usuário Marketing</div>
-  <div v-else>Usuário User</div>
-
-  <img
-    alt="Vue logo" 
-    src="./assets/logo.png"
-  >
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-    TheHeader
-  },
   data() {
     return {
-      //aqui é um objeto
-      showHeader: true, //variaveis reativas
-      firstName: 'Samuel',
-      lastName: 'Dourado',
-      showName: true,
-      acessLevel: 'marketing', 
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
     }
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
+
+.todos-item {
+    background-color: #3d3d3d;
+    color: #fff;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
 </style>
