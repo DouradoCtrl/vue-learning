9730a5d60ee98a343e684a93c755e49bac2188ef
DouradoCtrl <gdouradoestudante@gmail.com>
2026-02-04 16:09:21 -0300

    add: criando componente the header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="header">
+        Header Component
+    </header>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .header {
+        background-color: #000000;
+        padding: 20px;
+        color: #FFFFFF;
+        text-align: center;
+        font-size: 24px;
+        font-weight: bold;
+    }
+</style>
