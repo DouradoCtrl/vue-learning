0d7589e76d05f6d2a658e446d0adf269937054df
DouradoCtrl <gdouradoestudante@gmail.com>
2026-02-05 11:42:16 -0300

    AULA 08: Como utilizar v-model na aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,76 @@
 <template>
-  <!-- Classes dinâmicas css, passando um objeto -->
-  <h1 :class="{'title': true, 'title-home': isHome}">Curso Vue 3</h1> 
+  <div>
+    One-way data binding <br>
+    Two-Way data binding <br>
+    v-model -> formulários
 
-  <!-- Passando um array e dentro de um array um objeto -->
-  <p :class="pClass">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. ipsum dolor sit amet, consectetur adipiscing elit.
-    Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
-  </p>
+    <br><br>
+    <label>Nome</label><br>
+    <input 
+      v-model="name"
+      type="text"
+    ><br>
+    {{ name }}
+    
+    <br><br>
 
-  <!-- Passando objeto diretamente no style usando v-bind -->
-  <p :style="pStyle">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. ipsum dolor sit amet, consectetur adipiscing elit.
-    Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
-  </p>
-  
-  <div 
-    v-for="(obj, index) in todos" 
-    v-bind:key="obj.id"
-    class="todos-item"
-  > 
-    <!-- : ou v-bind = uma instrução para dizer que esse atributo vai ser um atributo dinâmico. -->
-    <img :src="obj.imgUrl" v-if="obj.imgUrl" :alt="imgAlt">
-    {{ index + 1 }} - {{ obj.title }}
+    <label>Sports</label><br>
+    <select v-model='sports'>
+      <option value="">Escolha</option>
+      <option value="soccer">Futebol</option>
+      <option value="basketball">basquete</option>
+      <option value="tenis">Tênis</option>
+    </select> <br>
+    {{ sports }}
+
+    <br><br>
+    <label>Newsletter</label><br>
+    <input 
+      v-model="newsletter"
+      type="radio"
+      value="sim"
+    /> Sim
+
+    <input 
+      v-model="newsletter"
+      type="radio"
+      value="nao"
+    /> Não <br>
+
+    {{ newsletter }}
+
+    <br><br>
+
+    <label>Contrato</label><br>
+    <input
+      v-model="contract"
+      type="checkbox"
+    /> Aceito os termos... <br>
+    {{ contract }}
+
+    <br><br>
+    <label>Selecione as cores</label><br>
+    <input
+      v-model="colors" 
+      type="checkbox"
+      value="yellow"
+    > Amarelo <br>
+
+    <input
+      v-model="colors" 
+      type="checkbox"
+      value="green"
+    > Verde <br>
+
+    <input
+      v-model="colors" 
+      type="checkbox"
+      value="red"
+    > Vermelho <br>
+
+    {{ colors }}
+
+
   </div>
 </template>
 
@@ -37,45 +80,12 @@ export default {
   name: 'App',
   data() {
     return {
-      isHome: true,
-      classVar: 'title',
-      pClass: ['text', 'text-home'],
-      imgAlt: 'Imagem de exemplo',
-      pStyle: {color: 'white', backgroundColor: 'black', padding: '10px', fontWeight: 'bold'},
-      todos: [
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "completed": false,
-          "imgUrl": 'https://placehold.co/150x150',
-          
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-        },
-        {
-          "userId": 1,
-          "id": 5,
-          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-          "completed": false
-        }
-      ]
+      name: 'Pedro',
+      sports: '',
+      newsletter: 'sim',
+      contract: true,
+      colors: [],
+
     }
   }
 }
@@ -88,30 +98,4 @@ export default {
   color: #2c3e50;
   margin: 60px;
 }
-
-.todos-item {
-    background-color: #3d3d3d;
-    color: #fff;
-    padding: 10px;
-    margin-bottom: 10px;
-  }
-
-.title {
-    color: #42b983;
-} 
-  
-.title-home {
-    font-size: 70px;
-    color: #ff00b3;
-}
-
-.text {
-    color: gray;
-    font-weight: bold;
-}
-
-.text-home {
-    color: #f37200;
-}
-
 </style>
