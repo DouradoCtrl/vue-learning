8ec27eaa16afe5cb24f5f9b8cf29f32c45774bdc
DouradoCtrl <gdouradoestudante@gmail.com>
2026-02-05 16:54:32 -0300

    AULA 10: ciclo de vida #1

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
   <div>
-    <label>Nome: </label>
-    <input
-      v-model="name"
-      type="text"
-    > {{ name }}
-
-    <br><br>
-    <label>Objeto Watch</label><br>
-
-    <input
-      v-model="user.first_name" 
-      type="text"
-    > <br>
-
-    <input
-      v-model="user.last_name" 
-      type="text"
-    > <br>
-
-    {{ fullName }}
+    <h1>Hello Word</h1>
+    {{ name }}
   </div>
 </template>
 
@@ -29,32 +11,71 @@ export default {
   name: 'App',
   data() {
     return {
-      name: 'Samuel',
-      user: {
-        first_name: '',
-        last_name: '',
-      }
+      name: 'Steve Jones'
     }
+  },
+
+  /* 
+    -== Ciclo de vida ==-
+    -= Criação =-
+      - Preparar o componente
+      - Ajax, incializar algumas variáveis
+      - Não tem acesso ao template (DOM)
+    -= Montagem =-
+      - Inicializar uma lib externa (new Lib())
+      - Precisa de acesso ao template (DOM)
+      - Ter acesso ao template (DOM)
+    -= Atualização =-
+      - Debug
+      - Update
+    -= Desmontagem =-
+      - Remove tudo o que for necessário
+      - Para liberar memória
+
+    
+  */
+
+  // -== HOOKS
+  beforeCreate() {
+    console.log('beforeCreate');
+    console.log('Estado:', this.name);
+    console.log('DOM', this.$el);
+  },
+
+  created() {
+    console.log('created');
+    console.log('Estado:', this.name);
+    console.log('DOM', this.$el);
+  },
+
+  beforeMount() {
+    console.log('beforeMount');
+    console.log('Estado:', this.name);
+    console.log('DOM', this.$el);
+  },
+
+  mounted() {
+    console.log('mounted');
+    console.log('Estado:', this.name);
+    console.log('DOM', this.$el);
+  },
+
+  beforeUnmount() {
+    console.log('beforeUnmount');
+    console.log('Estado:', this.name);
+    console.log('DOM', this.$el);
+  },
+
+  unmounted() {
+    console.log('unmounted');
+    console.log('Estado:', this.name);
+    console.log('DOM', this.$el);
   },
 
   watch: {
-    name() {
-      console.log("Nome alterado");
-    },
-
-    user: {
-      handler() {
-        console.log("Nome em objeto alterado");
-      },
-
-      deep: true
-    }
   },
   
   computed: {
-    fullName() {
-      return `${this.user.first_name} ${this.user.last_name}`
-    }
   },
   
   methods: {
@@ -69,9 +90,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin: 60px;
-}
-
-span {
-  font-weight: bold;
 }
 </style>
