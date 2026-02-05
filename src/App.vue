579bcca2a362be7a2a2dd1feb66e16aaa79856cc
DouradoCtrl <gdouradoestudante@gmail.com>
2026-02-05 14:53:45 -0300

    AULA 10: Todo do list com conceitos de 'computed'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
     {{ fullName }}
+    <h1>Todos em aberto</h1>
+    <div
+      v-for="todo in uncompletedTodos"
+      :key="todo.id"
+    >
+      {{ todo.title }} <!-- <span>{{ todo.completed }}</span> -->
+    </div>
+
+    <h1>Todos completas</h1>
+    <div
+      v-for="todo in completedTodos"
+      :key=todo.id
+    >
+      {{ todo.title }} <!-- <span>{{ todo.completed }}</span> -->
+    </div>
+
+    <h1>Todos</h1>
+    <div 
+      v-for="todo in todos"
+      :key=todo.id
+    >
+      <input
+        v-model="todo.completed" 
+        type="checkbox"
+      >
+      {{ todo.title }} <!-- <span>{{ todo.completed }}</span> -->
+    </div>
   </div>
 </template>
 
@@ -13,13 +40,50 @@ export default {
       user: {
         first_name: "Samuel",
         last_name: "Dourado"
+      },
+      todos: [
+      {
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+      },
+      {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+      },
+      {
+        "userId": 1,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+      },
+      {
+        "userId": 1,
+        "id": 4,
+        "title": "et porro tempora",
+        "completed": true
+      },
+      {
+        "userId": 1,
+        "id": 5,
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
       }
+    ]
     }
   },
 
   computed: {
     fullName() {
       return `${this.user.first_name} ${this.user.last_name}`
+    },
+    completedTodos() {
+      return this.todos.filter(todo => todo.completed );
+    },
+    uncompletedTodos() {
+      return this.todos.filter(todo => !todo.completed);
     }
   }
 }
@@ -31,5 +95,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin: 60px;
+}
+
+span {
+  font-weight: bold;
 }
 </style>
