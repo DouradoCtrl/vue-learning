f588930224f22a3ad2d3556b512f115237f330bb
DouradoCtrl <gdouradoestudante@gmail.com>
2026-02-06 15:36:00 -0300

    AULA 15: Exemplo utilizando props sem computed

---
+++ src/components/BaseAlert.vue
@@ -0,0 +1,37 @@
+<template>
+    <div :class="['alert', {
+        'alert-sucess': variant === 'success', 
+        'alert-danger': variant === 'danger',
+        }]">
+        Formulário enviado com sucesso
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['variant'],
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+    .alert {
+        background-color: bisque;
+        padding: 1rem;
+        font-weight: bold;
+        border-radius: 0.5rem;
+    }
+
+    .alert-sucess {
+        background-color: green;
+        color: #FFF;
+    }
+
+    .alert-danger {
+        background-color: red;
+        color: #FFF;
+    }
+</style>
